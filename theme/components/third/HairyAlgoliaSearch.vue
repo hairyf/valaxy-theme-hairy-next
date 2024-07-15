36525dbebeff13c64c13335ec336ad6d85dbf0e7
Mr.Mao <951416545@qq.com>
2024-07-16 00:31:50 +0800

    feat: update search change

--- theme/components/third/HairyAlgoliaSearch.vue
+++ theme/components/third/HairyAlgoliaSearch.vue
@@ -1,17 +1,3 @@
-<script lang="ts" setup>
-import * as addonAlgolia from 'valaxy-addon-algolia'
-import { isEmptyAddon } from 'valaxy'
-import { whenever } from '@vueuse/core'
-
-if (isEmptyAddon(addonAlgolia))
-  throw new Error('Algolia addon is not installed')
-
-const { loaded, load, dispatchEvent } = addonAlgolia.useAddonAlgolia()
-
-whenever(loaded, dispatchEvent)
-</script>
-
 <template>
-  <AlgoliaSearchBox v-if="loaded" class="VPNavBarSearch" />
-  <HairySearchBtnDisplay v-else @click="load()" />
+  <AlgoliaSearchBox class="VPNavBarSearch" />
 </template>
